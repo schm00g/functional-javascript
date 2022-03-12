504666016d99706b80345d35d1b0532d6bb34582
schm00g <hk8igureu@relay.firefox.com>
2022-03-12 02:27:33 +0000

    destructure

--- destructuring.js
+++ destructuring.js
@@ -6,4 +6,19 @@ let animal = {
 
 let { species, sound } = animal;
 
-console.log(`The species ${species} makes the sound "${sound}"`);
+console.log(`The patient ${species} makes the sound "${sound}"`);
+
+makeSound({
+    species: 'dog',
+    weight: 23,
+    sound: 'woof'
+})
+
+makeSound({
+    weight: 55,
+    sound: 'meow'
+})
+
+function makeSound({species = 'animal', sound}){
+    console.log(`The ${species} goes "${sound}"`);
+}
