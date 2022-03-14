e8fee66d84984dedc916f5aa9db0acea9b2b921e
schm00g <hk8igureu@relay.firefox.com>
2022-03-14 16:26:39 +0000

    recursive make tree

--- recursion.js
+++ recursion.js
@@ -7,3 +7,25 @@ let countDownFrom = (num) => {
 }
 
 countDownFrom(20);
+
+let categories = [
+    { id: 'animals', 'parent': null },
+    { id: 'mammals', 'parent': 'animals' },
+    { id: 'cats', 'parent': 'mammals' },
+    { id: 'dogs', 'parent': 'mammals' },
+    { id: 'chihuahua', 'parent': 'dogs' },
+    { id: 'labrador', 'parent': 'dogs' },
+    { id: 'persian', 'parent': 'cats' },
+    { id: 'siamese', 'parent': 'cats' },
+]
+
+function makeTree(categories, parent){
+    let node = {}
+    categories
+        .filter(c => c.parent === parent)
+        .forEach(c => node[c.id] = 
+            makeTree(categories, c.id))
+    return node
+}
+
+console.log(makeTree(categories, null));
