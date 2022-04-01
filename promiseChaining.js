547036f7de50796198f8357173854ad2084e2368
schm00g <hk8igureu@relay.firefox.com>
2022-04-01 19:33:21 +0100

    parsed data method

--- promiseChaining.js
+++ promiseChaining.js
@@ -1,10 +1,17 @@
 let fetchData = function(data){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('Processing Data Complete');
+            console.log('Fetching Data Complete');
             resolve({id: 1, message: 'nice work!'});
         }, 2000);
     });
 }
 
-// TODO: https://www.youtube.com/watch?v=zu6I2FXakLI
+let parseData = function(data){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let parsedOutput = `Parsed the data for id: ${data.id} with message: ${data.message}`;
+            resolve({parsed: parsedOutput});
+        });
+    })
+}
